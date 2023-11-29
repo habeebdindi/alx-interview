1baaffa2e64d5f22531f8b2a460f1856a701fae5
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-11-29 12:56:07 +0100

    Add first solution

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" This module contains a function"""
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    outer = []
+    for i in range(n):
+        inner = [1]
+        if i == 0:
+            outer.append(inner)
+        else:
+            if i > 1:
+                for j in range(len(outer[i - 1]) - 1):
+                    x = outer[i - 1][j] + outer[i - 1][j + 1]
+                    inner.append(x)
+                    j = j + 1
+            inner.append(1)
+            outer.append(inner)
+        i = i + 1
+    return outer
