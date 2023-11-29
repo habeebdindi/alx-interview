a0e13606b1df8643584c5fb3c5af8ddf703ccf51
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-11-29 13:05:38 +0100

    Document function

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,6 +2,7 @@
 """ This module contains a function"""
 
 def pascal_triangle(n):
+    """returns a list of lists of integers representing the Pascal's triangle of n:"""
     if n <= 0:
         return []
     outer = []
