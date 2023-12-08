60001ce68a8dce255cee5930ff664e42dff6103d
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-12-08 06:46:30 +0100

    Replace booleans with string

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,13 +5,13 @@
 def canUnlockAll(boxes):
     """a method that determines if all the boxes can be opened"""
 
-    [box.append(False) for box in boxes[1:]]
-    boxes[0].append(True)
+    [box.append('F') for box in boxes[1:]]
+    boxes[0].append('T')
 
     for i in range(len(boxes)):
         for j in range(len(boxes[i])):
             for k in range(len(boxes)):
-                if boxes[i][j] == k and False in boxes[k] and k != i:
-                    boxes[k][boxes[k].index(False)] = True
-    check = [True in item for item in boxes]
+                if boxes[i][j] == k and 'F' in boxes[k] and k != i:
+                    boxes[k][boxes[k].index('F')] = 'T'
+    check = ['T' in item for item in boxes]
     return all(check)
