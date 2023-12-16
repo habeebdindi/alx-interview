57d0e215d4170e2361edec897319e11a948e63b7
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-12-16 16:56:03 +0100

    Use sympy

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python3
 """This module contains various functions"""
+from sympy import factorint
 
+
+"""
 
 def get_factors(n):
-    """This function returns the factors of a number"""
     return [i for i in range(1, int(n) + 1) if n % i == 0]
 
 
 def get_prime_factors(factors):
-    """Returns the prime factors in a list"""
     return [key for key in factors if len(get_factors(key)) < 3]
 
 
 def no_of_times(n, div):
-    """Retruns the number of times a number divides a number"""
-    if n % div != 0:
+        if n % div != 0:
         return 0
     return 1 + no_of_times(n / div, div)
+
+"""
 
 
 def minOperations(n):
     """calculates the fewest number of operations needed to result in exactly
     n H characters in the file."""
+    """
     if n < 2:
         return 0
     factors = get_factors(n)
@@ -29,5 +32,6 @@ def minOperations(n):
     p_factor_occurence = {k: no_of_times(n, k) for k in prime_factors[1:]}
     minOps = 0
     for k, v in p_factor_occurence.items():
-        minOps += k * v
-    return minOps
+        minOps += k * v """
+    prime_factors = factorint(n)
+    return sum(k * v for k, v in prime_factors.items())
