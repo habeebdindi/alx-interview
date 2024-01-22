31391858d6027c16a48891411b67feddf5d97e51
Habeeb Dindi <habeebullahdindi@gmail.com>
2024-01-22 02:53:02 +0100

    Await promises

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,25 +4,35 @@ const request = require('request');
 const movieId = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-async function starWars () {
-  await request.get(url, (error, response, body) => {
-    if (error) {
-      console.log(error);
-    } else {
-      const data = JSON.parse(body);
-      const characters = data.characters;
-      characters.map(async (link) => {
-        await request.get(link, (error, response, body) => {
-          if (error) {
-            console.log(error);
-          } else {
-            const data = JSON.parse(body);
-            console.log(data.name);
-          }
-        });
-      });
-    }
+function promisifiedRequest (url) {
+  return new Promise((resolve, reject) => {
+    request.get(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(body);
+      }
+    });
   });
+}
+
+async function starWars () {
+  try {
+    const body = await promisifiedRequest(url);
+    const data = JSON.parse(body);
+    const characters = data.characters;
+
+    const namesPromises = characters.map(async (link) => {
+      const characterBody = await promisifiedRequest(link);
+      const characterData = JSON.parse(characterBody);
+      return characterData.name;
+    });
+
+    const names = await Promise.all(namesPromises);
+    names.forEach(name => { console.log(name); });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 starWars();
