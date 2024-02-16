7659de9d9a1c96e2ca33fdb70b70175a4c5314e8
Habeeb Dindi <habeebullahdindi@gmail.com>
2024-02-16 03:43:02 +0100

    Add solution

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""This module contains two functions
+"""
+
+
+def primes(n):
+    """Return list of prime numbers between 1 and n inclusive using
+       sieve of Eratosthenes.
+
+       Args:
+           n (int): upper boundary of range. lower boundary is always 1
+    """
+    prime = []
+    sieve = [True] * (n + 1)
+    for p in range(2, n + 1):
+        if (sieve[p]):
+            prime.append(p)
+            for i in range(p, n + 1, p):
+                sieve[i] = False
+    return prime
+
+
+def isWinner(x, nums):
+    """
+    Determines winner of Prime Game
+    Args:
+        x (int): no. of rounds of game
+        nums (int): upper limit of range for each round
+    Return:
+        Name of winner (Wallace or Matt) or None if winner cannot be found
+    """
+    if x is None or nums is None or x == 0 or nums == []:
+        return None
+    Wallace = Matt = 0
+    for i in range(x):
+        prime = primes(nums[i])
+        if len(prime) % 2 == 0:
+            Matt += 1
+        else:
+            Wallace += 1
+    if Wallace > Matt:
+        return 'Wallace'
+    elif Matt > Wallace:
+        return 'Matt'
+    return None
